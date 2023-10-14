e14d361bc849016280bf6acdff9f64e1dcdad822
Samrat Alam <samrat.alam@reddotdigitalit.com>
2023-10-14 11:35:42 +0600

    added

---
+++ src/main/java/com/example/demosecuritypractice/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demosecuritypractice.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+
+}
