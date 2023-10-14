651f3cbcab5a72fa629c9308dc8860c21ef860d4
Samrat Alam <samrat.alam@reddotdigitalit.com>
2023-10-14 12:04:07 +0600

    added

--- src/main/java/com/example/demosecuritypractice/entity/User.java
+++ src/main/java/com/example/demosecuritypractice/entity/User.java
@@ -4,18 +4,55 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String userName;
     private String email;
     private String password;
+    private Boolean isExpired;
+    private Boolean isAccountLocked;
+    private Boolean isCredentialsExpired;
+    private Boolean isEnable;
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return !this.isExpired;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !this.isAccountLocked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return !this.isCredentialsExpired;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return this.isEnable;
+    }
 }
